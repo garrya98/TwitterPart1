cc612abb4a07f0de80894cf4f13180c0557a8623
Garry Jamel Archbold Humphries <garrya@fb.com>
2017-06-26 17:13:16 -0700

    I like turtles

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/TweetAdapter.java
@@ -0,0 +1,103 @@
+package com.codepath.apps.restclienttemplate.models;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.text.format.DateUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.codepath.apps.restclienttemplate.R;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * Created by garrya on 6/26/17.
+ */
+
+public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.ViewHolder> {
+    private List<Tweet> mTweets;
+    Context context;
+
+    public TweetAdapter(Context contextTurtle, List<Tweet> turtle) {
+        mTweets = turtle;
+        context = contextTurtle;
+    }
+
+
+    //for each row, inflate the layout and cache references into VewHolder
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View tweetView = inflater.inflate(R.layout.item_tweet, parent, false);
+        ViewHolder viewHolder = new ViewHolder(tweetView);
+        return viewHolder;
+    }
+
+    //bind the values based on the position of the element
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // get the data according to position
+            Tweet tweet = mTweets.get(position);
+
+        //populate the views according to this data
+        holder.tvUsername.setText(tweet.user.name);
+        holder.tvBody.setText(tweet.body);
+        holder.tvTimeStamp.setText(getRelativeTimeAgo(tweet.createdAt));
+
+        Glide.with(context).load(tweet.user.profileImageUrl).into(holder.ivPorfileImage);
+    }
+
+
+    //create ViewHolder class
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView ivPorfileImage;
+        public TextView tvUsername;
+        public TextView tvBody;
+        public TextView tvTimeStamp;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            // perform findViewById lookups
+
+            ivPorfileImage = (ImageView) itemView.findViewById(R.id.ivProfileImage);
+            tvUsername = (TextView) itemView.findViewById(R.id.tvUsername);
+            tvBody = (TextView) itemView.findViewById(R.id.tvBody);
+            tvTimeStamp = (TextView) itemView.findViewById(R.id.tvTimeStamp);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mTweets.size();
+    }
+    public String getRelativeTimeAgo(String rawJsonDate) {
+        String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
+        SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
+        sf.setLenient(true);
+
+        String relativeDate = "";
+        try {
+            long dateMillis = sf.parse(rawJsonDate).getTime();
+            relativeDate = DateUtils.getRelativeTimeSpanString(dateMillis,
+                    System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS).toString();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return relativeDate;
+    }
+
+}
