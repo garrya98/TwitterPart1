81955cc15f15c5fe31cfbbda05c1b072b79ef2fd
Garry Jamel Archbold Humphries <garrya@fb.com>
2017-07-06 17:10:53 -0700

    updated README

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/fragments/HomeTimelineFragment.java
@@ -0,0 +1,8 @@
+package com.codepath.apps.restclienttemplate.fragments;
+
+/**
+ * Created by garrya on 7/3/17.
+ */
+
+public class HomeTimelineFragment extends TweetsListFragment {
+}
---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/fragments/TweetsListFragment.java
@@ -0,0 +1,10 @@
+package com.codepath.apps.restclienttemplate.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by garrya on 7/3/17.
+ */
+
+public class TweetsListFragment extends Fragment {
+}
